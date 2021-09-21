3ce0a5fd4aa0366a22a4567f381357e62a10be6c
羅雲聘 <pzps94038@yahoo.com.tw>
2021-09-21 16:52:45 +0800

    調整shared管理

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeModule } from './home/home.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedMaterialModule } from './shared-material/shared-material.module';
-
+import { SharedMaterialModule } from './shared/shared-material.module';
 
 @NgModule({
   declarations: [
